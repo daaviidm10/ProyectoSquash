1e604e27d02acdd17082eaa6a2633be3096af9eb
britza <bzegarraflores@danielcastelao.org>
2021-02-05 09:53:12 +0100

    segundo commit

--- src/com/company/ramaColaboradorar.java
+++ src/com/company/ramaColaboradorar.java
@@ -3,5 +3,6 @@ package com.company;
 public class ramaColaboradorar {
     public void colaborador(){
         System.out.println("primer commit");
+        System.out.println("segundo commit");
     }
 }
